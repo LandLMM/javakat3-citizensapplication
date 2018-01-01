e5d8df2e19a55e9be2af3f4a1450479128b60e9d
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-01 18:22:08 +0100

    Refactored decorator to use streams

--- src/main/java/pl/sdacademy/citizens/PersonListDecorator.java
+++ src/main/java/pl/sdacademy/citizens/PersonListDecorator.java
@@ -3,6 +3,7 @@ package pl.sdacademy.citizens;
 import pl.sdacademy.citizens.model.Person;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PersonListDecorator extends ArrayList<Person> {
 
@@ -11,54 +12,27 @@ public class PersonListDecorator extends ArrayList<Person> {
     }
 
     public Map<String, Long> createNameSummary() {
-        Map<String, Long> nameCount = new HashMap<>();
-        for (Person person : this) {
-            String name = person.getName();
-            Long peopleWithTheSameNameCount = nameCount.getOrDefault(name, 0L);
-            peopleWithTheSameNameCount++;
-            nameCount.put(name, peopleWithTheSameNameCount);
-        }
-        return nameCount;
+        return stream().collect(Collectors.groupingBy(Person::getName, Collectors.counting()));
     }
 
-
     public Map<String,Long> countByLastName() {
-        Map<String,Long> countedByLastName = new HashMap<>();
-        for (Person person : this) {
-            countedByLastName.merge(person.getLastName(), 1L, (previousCount, countToAdd) -> previousCount + countToAdd);
-        }
-        return countedByLastName;
+        return stream().collect(Collectors.groupingBy(Person::getLastName, Collectors.counting()));
     }
 
     public Map<String,List<Person>> groupByName() {
-        Map<String,List<Person>> groupedByName = new HashMap<>();
-        for (Person person : this) {
-            List<Person> listOfPeopleWithTheSameName = groupedByName.getOrDefault(person.getName(), new ArrayList<>());
-            listOfPeopleWithTheSameName.add(person);
-            groupedByName.put(person.getName(), listOfPeopleWithTheSameName);
-        }
-        return groupedByName;
+        return stream().collect(Collectors.groupingBy(Person::getName, Collectors.toList()));
     }
 
     public List<Person> filterWithAgeBetween35and55() {
-        List<Person> filteredPeople = new ArrayList<>();
-        for (Person person : this) {
-            Integer age = person.getAge();
-            if (age >= 35 && age <= 55) {
-                filteredPeople.add(person);
-            }
-        }
-        return filteredPeople;
+        return stream().filter(person -> person.getAge() >= 35).filter(person -> person.getAge() <= 55).collect(Collectors.toList());
     }
 
     public Long countPossiblyRetired() {
-        Long possiblyRetired = 0L;
-        for (Person person : this) {
-            if (possiblyRetiredFemale(person) || possiblyRetiredMale(person)) {
-                possiblyRetired++;
-            }
-        }
-        return possiblyRetired;
+        return stream().filter(this::canBeRetired).count();
+    }
+
+    private boolean canBeRetired(Person person) {
+        return possiblyRetiredFemale(person) || possiblyRetiredMale(person);
     }
 
     private boolean possiblyRetiredFemale(Person person) {
