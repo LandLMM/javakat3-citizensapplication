bf7b93b742d64dbecf2a23c5c6f0318ecb18e28a
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-01 21:17:26 +0100

    Validation without tests. Tests will be implemented during refactoring.

--- src/main/java/pl/sdacademy/citizens/PersonReader.java
+++ src/main/java/pl/sdacademy/citizens/PersonReader.java
@@ -15,17 +15,35 @@ public class PersonReader {
         List<Person> persons = new ArrayList<>();
         long start = System.currentTimeMillis();
         for (CsvLine csvLine : csvLines) {
-            persons.add(
-                    Person.builder()
-                            .id(Long.parseLong(csvLine.getElementAt(0)))
-                            .name(csvLine.getElementAt(1))
-                            .lastName(csvLine.getElementAt(2))
-                            .sex(csvLine.getElementAt(3))
-                            .birthDate(csvLine.getElementAt(4))
-                            .build());
+            Person person = Person.builder()
+                    .id(Long.parseLong(csvLine.getElementAt(0)))
+                    .name(csvLine.getElementAt(1))
+                    .lastName(csvLine.getElementAt(2))
+                    .sex(csvLine.getElementAt(3))
+                    .birthDate(csvLine.getElementAt(4))
+                    .build();
+            if (isValid(person)) {
+                persons.add(person);
+            }
         }
         long stop = System.currentTimeMillis();
         System.out.println("Converted " + persons.size() + " in " + (stop - start) + " ms");
         return persons;
+    }
+
+    private boolean isValid(Person person) {
+        if (person.getName() == null || person.getName().length() <= 2) {
+            return false;
+        }
+        if (person.getLastName() == null || person.getLastName().length() <= 2) {
+            return false;
+        }
+        if (!("F".equals(person.getSex()) || "M".equals(person.getSex()))) {
+            return false;
+        }
+        if (person.getBirthDate().getTime() > System.currentTimeMillis()) {
+            return false;
+        }
+        return person.getAge() > 18;
     }
 }
