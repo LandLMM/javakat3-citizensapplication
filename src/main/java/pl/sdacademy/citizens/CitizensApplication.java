fb161cf0e70c39f54abe5f5ed00b597a8912fff4
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-01 18:17:24 +0100

    Moved methods to decorator. Loops were modified to use "this" (the decorator values)

--- src/main/java/pl/sdacademy/citizens/CitizensApplication.java
+++ src/main/java/pl/sdacademy/citizens/CitizensApplication.java
@@ -22,70 +22,12 @@ public class CitizensApplication {
     public void process() throws ParseException {
         File personFile = new File(getClass().getClassLoader().getResource("person.csv").getFile());
         List<Person> people = personReader.readFromFile(personFile);
+        PersonListDecorator personListDecorator = new PersonListDecorator(people);
 
-        Map<String, Long> nameSummary = createNameSummary(people);
+        Map<String, Long> nameSummary = personListDecorator.createNameSummary();
         // sample: how to print results to the console
 //        for (Map.Entry<String, Long> nameCount : nameSummary.entrySet()) {
 //            System.out.println("There are " + nameCount.getValue() + " persons with name " + nameCount.getKey());
 //        }
-    }
-
-    private Map<String, Long> createNameSummary(List<Person> people) {
-        Map<String, Long> nameCount = new HashMap<>();
-        for (Person person : people) {
-            String name = person.getName();
-            Long peopleWithTheSameNameCount = nameCount.getOrDefault(name, 0L);
-            peopleWithTheSameNameCount++;
-            nameCount.put(name, peopleWithTheSameNameCount);
-        }
-        return nameCount;
-    }
-
-
-    public Map<String,Long> countByLastName(List<Person> personList) {
-        Map<String,Long> countedByLastName = new HashMap<>();
-        for (Person person : personList) {
-            countedByLastName.merge(person.getLastName(), 1L, (previousCount, countToAdd) -> previousCount + countToAdd);
-        }
-        return countedByLastName;
-    }
-
-    public Map<String,List<Person>> groupByName(List<Person> personList) {
-        Map<String,List<Person>> groupedByName = new HashMap<>();
-        for (Person person : personList) {
-            List<Person> listOfPeopleWithTheSameName = groupedByName.getOrDefault(person.getName(), new ArrayList<>());
-            listOfPeopleWithTheSameName.add(person);
-            groupedByName.put(person.getName(), listOfPeopleWithTheSameName);
-        }
-        return groupedByName;
-    }
-
-    public List<Person> filterWithAgeBetween35and55(List<Person> personList) {
-        List<Person> filteredPeople = new ArrayList<>();
-        for (Person person : personList) {
-            Integer age = person.getAge();
-            if (age >= 35 && age <= 55) {
-                filteredPeople.add(person);
-            }
-        }
-        return filteredPeople;
-    }
-
-    public Long countPossiblyRetired(List<Person> personList) {
-        Long possiblyRetired = 0L;
-        for (Person person : personList) {
-            if (possiblyRetiredFemale(person) || possiblyRetiredMale(person)) {
-                possiblyRetired++;
-            }
-        }
-        return possiblyRetired;
-    }
-
-    private boolean possiblyRetiredFemale(Person person) {
-        return "F".equals(person.getSex()) && person.getAge() >= 60;
-    }
-
-    private boolean possiblyRetiredMale(Person person) {
-        return "M".equals(person.getSex()) && person.getAge() >= 65;
     }
 }
--- src/main/java/pl/sdacademy/citizens/PersonListDecorator.java
+++ src/main/java/pl/sdacademy/citizens/PersonListDecorator.java
@@ -2,12 +2,70 @@ package pl.sdacademy.citizens;
 
 import pl.sdacademy.citizens.model.Person;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class PersonListDecorator extends ArrayList<Person> {
 
     public PersonListDecorator(Collection<? extends Person> c) {
         super(c);
+    }
+
+    public Map<String, Long> createNameSummary() {
+        Map<String, Long> nameCount = new HashMap<>();
+        for (Person person : this) {
+            String name = person.getName();
+            Long peopleWithTheSameNameCount = nameCount.getOrDefault(name, 0L);
+            peopleWithTheSameNameCount++;
+            nameCount.put(name, peopleWithTheSameNameCount);
+        }
+        return nameCount;
+    }
+
+
+    public Map<String,Long> countByLastName() {
+        Map<String,Long> countedByLastName = new HashMap<>();
+        for (Person person : this) {
+            countedByLastName.merge(person.getLastName(), 1L, (previousCount, countToAdd) -> previousCount + countToAdd);
+        }
+        return countedByLastName;
+    }
+
+    public Map<String,List<Person>> groupByName() {
+        Map<String,List<Person>> groupedByName = new HashMap<>();
+        for (Person person : this) {
+            List<Person> listOfPeopleWithTheSameName = groupedByName.getOrDefault(person.getName(), new ArrayList<>());
+            listOfPeopleWithTheSameName.add(person);
+            groupedByName.put(person.getName(), listOfPeopleWithTheSameName);
+        }
+        return groupedByName;
+    }
+
+    public List<Person> filterWithAgeBetween35and55() {
+        List<Person> filteredPeople = new ArrayList<>();
+        for (Person person : this) {
+            Integer age = person.getAge();
+            if (age >= 35 && age <= 55) {
+                filteredPeople.add(person);
+            }
+        }
+        return filteredPeople;
+    }
+
+    public Long countPossiblyRetired() {
+        Long possiblyRetired = 0L;
+        for (Person person : this) {
+            if (possiblyRetiredFemale(person) || possiblyRetiredMale(person)) {
+                possiblyRetired++;
+            }
+        }
+        return possiblyRetired;
+    }
+
+    private boolean possiblyRetiredFemale(Person person) {
+        return "F".equals(person.getSex()) && person.getAge() >= 60;
+    }
+
+    private boolean possiblyRetiredMale(Person person) {
+        return "M".equals(person.getSex()) && person.getAge() >= 65;
     }
 }
