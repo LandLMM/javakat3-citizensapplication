afb2e3034a149df074a88b7572ab2503f2c6a6b0
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-02 22:01:42 +0100

    Added age validation rule

---
+++ src/main/java/pl/sdacademy/citizens/validation/PersonAdultAgeRule.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.citizens.validation;
+
+import pl.sdacademy.citizens.model.Person;
+
+import java.util.Optional;
+
+public class PersonAdultAgeRule implements ValidationRule {
+    @Override
+    public boolean isValid(Person person) {
+        Integer personAge = Optional.ofNullable(person).map(Person::getAge).orElse(-1);
+        return personAge >= 18;
+    }
+}
